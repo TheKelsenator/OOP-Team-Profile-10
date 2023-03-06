806c8fe560fca30db3d8594c597900a4366d22a6
Kelsey O'Kelley <km.okelley89@gmail.com>
2023-03-06 12:17:31 -0700

    index.js questions, some css

--- index.js
+++ index.js
@@ -1,3 +1,139 @@
+const Employee = require('../lib/employee');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
+const Manager = require('../lib/manager');
+
+const managerQuestions = [
+  {
+    name: 'managerName',
+    type: 'input',
+    message: 'Who is the team manager?',
+  },
+  {
+    name: 'managerID',
+    type: 'input',
+    message: "What is the manager's employee ID#?",
+  },
+  {
+    name: 'managerEmail',
+    type: 'input',
+    message: "What is the manager's email address?",
+  },
+  {
+    name: 'managerPhone',
+    type: 'input',
+    message: "What is the manager's office phone number?",
+  },
+];
+    // getName()
+    // getId()
+    // getEmail()
+    // getOfficePhone()
+    // getRole() Returns 'Manager'
+
+const addEmployee = [
+  {
+    name: 'addEmployee',
+    type: 'list',
+    message: 'Are there any members of the team that need to be added?',
+    choices: [
+      'Yes, an intern.',
+      'Yes, an engineer.',
+      'No, all team members have been added.'
+    ],
+  },
+];
+    // getName()
+    // getId()
+    // getEmail()
+    // getRole() Returns 'Employee'
+
+const internQuestions = [
+  {
+    name: 'internName',
+    type: 'input',
+    message: "What is the intern's name?",
+  },
+  {
+    name: 'internID',
+    type: 'input',
+    message: "What is the interns's employee ID#?",
+  },
+  {
+    name: 'internEmail',
+    type: 'input',
+    message: "What is the intern's email address?",
+  },
+  {
+    name: 'internSchool',
+    type: 'input',
+    message: "Where did the intern graduate from school?",
+  },
+];
+    // getName()
+    // getId()
+    // getEmail()
+    // getSchool
+    // getRole() Returns 'Intern'
+
+const engineerQuestions = [
+  {
+    name: 'engineerName',
+    type: 'input',
+    message: "What is the engineer's name?",
+  },
+  {
+    name: 'engineerID',
+    type: 'input',
+    message: "What is the engineer's employee ID#?",
+  },
+  {
+    name: 'engineerEmail',
+    type: 'input',
+    message: "What is the engineer's email address?",
+  },
+  {
+    name: 'engineerGithub',
+    type: 'input',
+    message: "What is the engineer's Github username?"
+  },
+];
+    // getName()
+    // getId()
+    // getEmail()
+    // getGithub()
+    // getRole() Returns 'Engineer'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // questions = {
 //   Who manages the team?
 //   What is the manager's Employee ID #?
